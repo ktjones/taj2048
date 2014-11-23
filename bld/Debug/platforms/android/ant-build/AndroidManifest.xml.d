bd0c4d1e9120c1afd9976621c50cbcd7bfaef342
ktjones <todd@ktjones.net>
2014-11-23 16:04:32 -0600

    0
    0

---
+++ bld/Debug/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+c:\Users\ktjones\Documents\Visual Studio 2013\Projects\2048\2048\bld\Debug\platforms\android\ant-build\AndroidManifest.xml : \
+c:\Users\ktjones\Documents\Visual Studio 2013\Projects\2048\2048\bld\Debug\platforms\android\AndroidManifest.xml \
---
+++ bld/Debug/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+c:\Users\ktjones\Documents\Visual Studio 2013\Projects\2048\2048\bld\Debug\platforms\android\ant-build\classes.dex : \
+c:\Users\ktjones\Documents\Visual Studio 2013\Projects\2048\2048\bld\Debug\platforms\android\ant-build\classes\io\cordova\taj2048\BuildConfig.class \
+c:\Users\ktjones\Documents\Visual Studio 2013\Projects\2048\2048\bld\Debug\platforms\android\ant-build\classes\io\cordova\taj2048\CordovaApp.class \
+c:\Users\ktjones\Documents\Visual Studio 2013\Projects\2048\2048\bld\Debug\platforms\android\ant-build\classes\io\cordova\taj2048\R$attr.class \
+c:\Users\ktjones\Documents\Visual Studio 2013\Projects\2048\2048\bld\Debug\platforms\android\ant-build\classes\io\cordova\taj2048\R$drawable.class \
+c:\Users\ktjones\Documents\Visual Studio 2013\Projects\2048\2048\bld\Debug\platforms\android\ant-build\classes\io\cordova\taj2048\R$string.class \
+c:\Users\ktjones\Documents\Visual Studio 2013\Projects\2048\2048\bld\Debug\platforms\android\ant-build\classes\io\cordova\taj2048\R$xml.class \
+c:\Users\ktjones\Documents\Visual Studio 2013\Projects\2048\2048\bld\Debug\platforms\android\ant-build\classes\io\cordova\taj2048\R.class \
+c:\Users\ktjones\Documents\Visual Studio 2013\Projects\2048\2048\bld\Debug\platforms\android\ant-build\dexedLibs\classes-ba4dd6b316c6ffc1ae954054a77ceeda.jar \
