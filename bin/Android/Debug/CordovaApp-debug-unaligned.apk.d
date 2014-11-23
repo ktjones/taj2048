a74bf78076fe6cec05d3f77df0a3f98e0face03e
ktjones <todd@ktjones.net>
2014-11-23 16:08:27 -0600

    1
    1

---
+++ bin/Android/Debug/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+c:\Users\ktjones\Documents\Visual Studio 2013\Projects\2048\2048\bld\Debug\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+c:\Users\ktjones\Documents\Visual Studio 2013\Projects\2048\2048\bld\Debug\platforms\android\ant-build\CordovaApp.ap_ \
+c:\Users\ktjones\Documents\Visual Studio 2013\Projects\2048\2048\bld\Debug\platforms\android\ant-build\classes.dex \
+C:\Users\ktjones\Documents\Visual Studio 2013\Projects\2048\2048\bld\Debug\platforms\android\CordovaLib\ant-build\classes.jar \
---
+++ bld/Debug/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\ktjones\Documents\Visual Studio 2013\Projects\2048\2048\bld\Debug\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\ktjones\Documents\Visual Studio 2013\Projects\2048\2048\bld\Debug\platforms\android\CordovaLib\AndroidManifest.xml \
